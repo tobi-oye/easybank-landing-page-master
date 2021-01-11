2758df01b57695dd9703bcc4a81cd97b80cd10cf
tobi-oye <tobiloba5000@gmail.com>
2021-01-11 21:58:36 +0100

    [bug]-mobile responsiveness nav

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-let modal = document.querySelector(".modal");
+let modal = document.querySelector("nav");
 let headerBtn = document.querySelector(".header-btn");
-
+let modalContent = document.querySelector("#modal-content");
 let btn = document.querySelector(".btn-hamburger");
 
 /**
  * function to display modal
  */
 function displayModal() {
-  if (modal.style.display == "none") {
-    modal.style.display = "block";
+  if (modal.className == "") {
+    modal.className = "modal";
+    modalContent.className = "modal-content";
     headerBtn.style.display = "none";
   } else {
-    modal.style.display = "none";
+    modal.className = "";
+    modalContent.className = "";
   }
 }
 
